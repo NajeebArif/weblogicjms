29be0528e3579e6aa8b4cc80f6f6b0f914f26bf0
Najeeb Arif <NajeebArif786@outlook.com>
2020-04-05 21:58:37 +0530

    Defining JmsTemplate Bean

--- src/main/java/narif/poc/wlseries/weblogicjms/config/JmsConfig.java
+++ src/main/java/narif/poc/wlseries/weblogicjms/config/JmsConfig.java
@@ -2,6 +2,7 @@ package narif.poc.wlseries.weblogicjms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
@@ -15,5 +16,12 @@ public class JmsConfig {
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
         return converter;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setDefaultDestinationName("jms/myTestQueue");
+        return jmsTemplate;
     }
 }
