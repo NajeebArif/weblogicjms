075c03332c252e4fecd827aa8127e2a3c5b08010
Najeeb Arif <NajeebArif786@outlook.com>
2020-04-05 22:03:42 +0530

    Adding no args cons

--- src/main/java/narif/poc/wlseries/weblogicjms/model/CustomMsg.java
+++ src/main/java/narif/poc/wlseries/weblogicjms/model/CustomMsg.java
@@ -2,12 +2,14 @@ package narif.poc.wlseries.weblogicjms.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CustomMsg {
 
     private Long id;
