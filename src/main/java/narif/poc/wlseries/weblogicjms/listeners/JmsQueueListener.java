40f7f6a4e990fecd20c65a077581f3d9c95eede9
Najeeb Arif <NajeebArif786@outlook.com>
2020-04-06 00:36:59 +0530

    Create JmsQueueListener.java

---
+++ src/main/java/narif/poc/wlseries/weblogicjms/listeners/JmsQueueListener.java
@@ -0,0 +1,24 @@
+package narif.poc.wlseries.weblogicjms.listeners;
+
+import narif.poc.wlseries.weblogicjms.model.CustomMsg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+
+@Component
+@Transactional
+public class JmsQueueListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JmsQueueListener.class.getName());
+
+    @JmsListener(destination = "jms/myTestQueue")
+    public void listenToMessages(@Valid CustomMsg customMsg){
+        String text = "MESSAGE RECEIVED AT: "+ LocalDateTime.now()+". MSG: "+customMsg;
+        LOGGER.info(text);
+    }
+}
