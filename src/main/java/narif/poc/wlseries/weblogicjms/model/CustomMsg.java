03f52284426ced8583ae9a039777cbf5df9514ef
Najeeb Arif <NajeebArif786@outlook.com>
2020-04-05 21:58:49 +0530

    Adding Bean Validation addition

--- src/main/java/narif/poc/wlseries/weblogicjms/model/CustomMsg.java
+++ src/main/java/narif/poc/wlseries/weblogicjms/model/CustomMsg.java
@@ -3,11 +3,17 @@ package narif.poc.wlseries.weblogicjms.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 public class CustomMsg {
 
     private Long id;
+
+    @NotNull
+    @NotEmpty
     private String message;
 
     @Override
