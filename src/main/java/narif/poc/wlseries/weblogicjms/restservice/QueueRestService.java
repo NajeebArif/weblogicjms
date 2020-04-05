cb8c2f3a8bccaa1724b273ea52dba677f2168fa7
Najeeb Arif <NajeebArif786@outlook.com>
2020-04-05 22:08:46 +0530

    Create QueueRestService.java

---
+++ src/main/java/narif/poc/wlseries/weblogicjms/restservice/QueueRestService.java
@@ -0,0 +1,32 @@
+package narif.poc.wlseries.weblogicjms.restservice;
+
+import narif.poc.wlseries.weblogicjms.model.CustomMsg;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@RestController("queue")
+public class QueueRestService {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @PostMapping
+    public String postMessage(@RequestBody @Valid CustomMsg customMsg){
+        prepareFinalMsgForDefaultQ(customMsg);
+        jmsTemplate.convertAndSend(customMsg);
+        return "Message Sent!";
+    }
+
+    private void prepareFinalMsgForDefaultQ(@RequestBody @Valid CustomMsg customMsg) {
+        @NotNull @NotEmpty String msg = customMsg.getMessage();
+        String finalMsg = msg + ". MSG FOR DEFAULT QUEUE.";
+        customMsg.setMessage(finalMsg);
+    }
+}
